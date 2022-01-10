49395081805ac635a0f03a7d32b562b1555439cf
LauriSalonen <lauri.salonen@student.careeria.fi>
2022-01-10 13:00:59 +0200

    koira esimerkki

---
+++ olio/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    class Class1
+    {
+        public string nimi;
+        public int ikä;
+        public string rotu;
+
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Koiran nimi: " + nimi + " Ikä: " + ikä + " Rotu :" + rotu);
+        }
+        public void Hauku()
+        {
+            Console.WriteLine("Hau Hau");
+        }
+    }
+}
--- olio/ConsoleApp1/Program.cs
+++ olio/ConsoleApp1/Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Class1 koira1 = new Class1();
+            Class1 koira2 = new Class1();
+
+            koira1.nimi = "Tapsa";
+            koira1.ikä = 6;
+            koira1.rotu = "Kultainen noutaja";
+            koira2.nimi = "Luca";
+            koira2.ikä = 6;
+            koira2.rotu = "Shih Zhu";
+            koira2.TulostaData();
+            koira2.Hauku();
+            koira1.TulostaData();
+            koira1.Hauku();
+
+            
         }
     }
 }
