73301bf16023b371f20d1088a426e1d028cdf434
LauriSalonen <lauri.salonen@student.careeria.fi>
2022-01-10 13:44:06 +0200

    Autoharjoitus

---
+++ olio/ConsoleApp2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public string tyyppi;
+        public string ajoneuvo;
+        public string nopeus;
+        public int renkaat;
+        public int hepat;
+
+
+
+        public void AjoneuvoData()
+        {
+            Console.WriteLine("Ajoneuvon tyyppi: " + tyyppi + " | Ajoneuvon malli: " + ajoneuvo + " | Nopeus: " + nopeus + " | Renkaat: " + renkaat + " | Hevosvoimat: " + hepat);
+        }
+    }
+}
---
+++ olio/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 ajoneuvo1 = new Class1();
+            Class1 ajoneuvo2 = new Class1();
+            ajoneuvo1.tyyppi = "Auto";
+            ajoneuvo1.ajoneuvo = "Mercedes Benz C63 AMG";
+            ajoneuvo1.nopeus = "324 KM/H";
+            ajoneuvo1.renkaat = 4;
+            ajoneuvo1.hepat = 426;
+            ajoneuvo2.tyyppi = "Vene";
+            ajoneuvo2.ajoneuvo = "Bugatti Niniette 66";
+            ajoneuvo2.nopeus = "80 KM/h";
+            ajoneuvo2.renkaat = 0;
+            ajoneuvo2.hepat = 960;
+
+            ajoneuvo1.AjoneuvoData();
+            Console.WriteLine();
+            ajoneuvo2.AjoneuvoData();
+        }
+    }
+}
